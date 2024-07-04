84a96cc323e2b1e59cd2641a568bdb1b1b9a408e
fels123 <kelengulienakhro@gmail.com>
2024-07-04 21:06:18 +0530

    added images

---
+++ app.js
@@ -0,0 +1,8 @@
+const cardArray = [
+  { name: "ice-cream", img: "./images/ice-cream.png" },
+  { name: "milkshake", img: "./images/milkshake.png" },
+  { name: "pizza", img: "./images/pizza.png" },
+  { name: "ice-cream", img: "./images/ice-cream.png" },
+  { name: "milkshake", img: "./images/milkshake.png" },
+  { name: "pizza", img: "./images/pizza.png" },
+];
